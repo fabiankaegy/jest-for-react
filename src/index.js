fb79e56f488c6f3375c343338b8ead210442fd0c
Fabian Kägy <mail@fabian-kaegy.de>
2021-01-07 18:33:39 +0100

    convert indentation to tabs

--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@ import React, { useState } from 'react';
 
 export function Counter(props) {
 
-    const { initialCount = 0 } = props;
+	const { initialCount = 0 } = props;
 
-    const [ count, setCount ] = useState( initialCount );
+	const [ count, setCount ] = useState( initialCount );
 
-    function increaseCount() {
-        setCount( count + 1 );
-    }
+	function increaseCount() {
+		setCount( count + 1 );
+	}
 
-    function decreaseCount() {
-        setCount( count - 1 );
-    }
+	function decreaseCount() {
+		setCount( count - 1 );
+	}
 
-    return (
-        <div className="counter">
-            <p>The current count is: <span data-testid="current-count">{count}</span></p>
-            <button onClick={ decreaseCount }>-</button>
-            <button onClick={ increaseCount }>+</button>
-        </div>
-    )
+	return (
+		<div className="counter">
+			<p>The current count is: <span data-testid="current-count">{count}</span></p>
+			<button onClick={ decreaseCount }>-</button>
+			<button onClick={ increaseCount }>+</button>
+		</div>
+	)
 }
