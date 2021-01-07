e5babf2ff9aa2495ed123e78f132e9652e0e6e52
Fabian Kägy <mail@fabian-kaegy.de>
2021-01-07 18:21:18 +0100

    add additional decrease test

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
 import { Counter } from '../index';
 
 describe( 'testing the test suite', () => {
+
+    afterEach( cleanup )
 
     test( 'it works', () => {
         expect( true ).toBe( true );
     } )
 
-    test( 'example', () => {
+    test( 'the increment button increases the current count', () => {
         const { getByTestId, getByText } = render(
             <Counter />
         );
 
         expect(getByTestId('current-count').textContent).toBe('0');
-        
         fireEvent.click( getByText('+') );
-        
         expect(getByTestId('current-count').textContent).toBe('1');
+
+    } );
+    
+    test( 'the decrement button decreases the current count', () => {
+        const { getByTestId, getByText } = render(
+            <Counter />
+        );
+
+        expect(getByTestId('current-count').textContent).toBe('0');
+        fireEvent.click( getByText('-') );
+        expect(getByTestId('current-count').textContent).toBe('-1');
 
     } );
 
